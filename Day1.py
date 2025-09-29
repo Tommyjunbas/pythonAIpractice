dafbabbe875c3bf898f95a3a363859737cea941b
Tommyjunbas <dfomin03@gmail.com>
2025-09-29 19:49:22 +0300

    Add files via upload
    some python stuff

---
+++ Day1.py
@@ -0,0 +1,14 @@
+print("shalom, ya nachal")
+name = "thomasos"
+hours_per_day = 2
+total_hours_per_month = hours_per_day * 30
+print(f"meine naame ist{name}")
+print(f"ich lerne {total_hours_per_month} stunden pro monat im python)")
+x = 15
+y = 10
+result = x * y + 10
+print(f"result: {result}")
+progress_today = ["download - python, git, vs code"]
+print("progress today:")
+for progress in progress_today:
+    print(f"{progress}")
