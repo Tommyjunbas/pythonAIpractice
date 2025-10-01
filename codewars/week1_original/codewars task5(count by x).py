81a2dafe5667791a779c4f6f27e88571287a88ac
Tommyjunbas <dfomin03@gmail.com>
2025-10-01 19:25:00 +0300

    Add files via upload

---
+++ codewars/week1_original/codewars task5(count by x).py
@@ -0,0 +1,5 @@
+def count_by(x, n):
+    bib = []
+    for i in range (n):
+        bib.append((i+1)*x)
+    return bib
---
+++ codewars/week1_original/codewars task6(remove every other).py
@@ -0,0 +1,6 @@
+def remove_every_other(my_list):
+    result = []
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            result.append(my_list[i])
+    reurn result
